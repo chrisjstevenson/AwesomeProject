18a2e6c06a9ccdc6bca08655587e8419f6ff759d
chrisjstevenson <chris.j.stevenson@gmail.com>
2018-03-22 21:27:06 -0500

    Updating project signing settings to automatic

--- App.js
+++ App.js
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginTop: 230,
+        //marginTop: 230,
         backgroundColor: '#F8F8FF'
     },
     buttonLeft: {
